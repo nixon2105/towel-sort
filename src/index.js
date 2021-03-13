b8bf0965ad56c12371e02a5527742acecac655aa
nixon2105 <radivilov1991@mail.ru>
2021-03-13 17:51:47 +0300

    feat: add resolve task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    if (matrix.length == 0) return [];
+    let result = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 != 0) {
+            matrix[i] = matrix[i].reverse();
+        }
+        for (let j = 0; j < matrix[i].length; j++) {
+            result.push(matrix[i][j]);
+        }
+    }
+    return result;
+};
